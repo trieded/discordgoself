472bd9f85ced1436ca400a34295e54002ca82e47
courtier <g59@tutanota.com>
2021-08-24 14:42:02 +0200

    delete commented out tests and add a new check

--- restapi_test.go
+++ restapi_test.go
@@ -25,63 +25,6 @@ func TestChannelMessageSend(t *testing.T) {
 	}
 }
 
-/*
-// removed for now, only works on BOT accounts now
-func TestUserAvatar(t *testing.T) {
-
-	if dg == nil {
-		t.Skip("Cannot TestUserAvatar, dg not set.")
-	}
-
-	u, err := dg.User("@me")
-	if err != nil {
-		t.Error("error fetching @me user,", err)
-	}
-
-	a, err := dg.UserAvatar(u.ID)
-	if err != nil {
-		if err.Error() == `HTTP 404 NOT FOUND, {"code": 0, "message": "404: Not Found"}` {
-			t.Skip("Skipped, @me doesn't have an Avatar")
-		}
-		t.Errorf(err.Error())
-	}
-
-	if a == nil {
-		t.Errorf("a == nil, should be image.Image")
-	}
-}
-*/
-
-/* Running this causes an error due to 2/hour rate limit on username changes
-func TestUserUpdate(t *testing.T) {
-	if dg == nil {
-		t.Skip("Cannot test logout, dg not set.")
-	}
-
-	u, err := dg.User("@me")
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-
-	s, err := dg.UserUpdate(envEmail, envPassword, "testname", u.Avatar, "")
-	if err != nil {
-		t.Error(err.Error())
-	}
-	if s.Username != "testname" {
-		t.Error("Username != testname")
-	}
-	s, err = dg.UserUpdate(envEmail, envPassword, u.Username, u.Avatar, "")
-	if err != nil {
-		t.Error(err.Error())
-	}
-	if s.Username != u.Username {
-		t.Error("Username != " + u.Username)
-	}
-}
-*/
-
-//func (s *Session) UserChannelCreate(recipientID string) (st *Channel, err error) {
-
 func TestUserChannelCreate(t *testing.T) {
 	if dg == nil {
 		t.Skip("Cannot TestUserChannelCreate, dg not set.")
@@ -91,12 +34,13 @@ func TestUserChannelCreate(t *testing.T) {
 		t.Skip("Skipped, DG_ADMIN not set.")
 	}
 
-	_, err := dg.UserChannelCreate(envAdmin)
+	ch, err := dg.UserChannelCreate(envAdmin)
 	if err != nil {
 		t.Errorf(err.Error())
 	}
-
-	// TODO make sure the channel was added
+	if ch == nil {
+		t.Error("channel nil")
+	}
 }
 
 func TestUserChannels(t *testing.T) {
